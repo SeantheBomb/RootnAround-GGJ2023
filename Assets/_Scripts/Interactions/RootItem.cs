3032f2d1914adb8c9e123b793aaaff251cfa3ee5
Sean the bomb <sean.feeser@gmail.com>
2023-02-04 11:41:25 -0500

    Houses prefabed and food has variants

--- Assets/_Scripts/Interactions/RootItem.cs
+++ Assets/_Scripts/Interactions/RootItem.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class RootItem : MonoBehaviour
 {
+
+    [SerializeField] GameObject[] itemViews;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        RandomizeView();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+    void RandomizeView()
+    {
+        SetView(Random.Range(0, itemViews.Length));
+    }
+
+    void SetView(int index)
+    {
+        for (int i = 0; i < itemViews.Length; i++)
+        {
+            itemViews[i].SetActive(i == index);
+        }
     }
 }
